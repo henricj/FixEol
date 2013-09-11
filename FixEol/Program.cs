113ad27da84444fec667e37a3136868daaec7844
henric <software@henric.org>
2013-09-10 19:16:41 -0700

    Initial import

---
+++ FixEol/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace FixEol
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                args = new[]
+                       {
+                           Environment.CurrentDirectory
+                       };
+            }
+
+            var transform = new EncodingAndEolTransform
+                            {
+                                //OutputBomPolicy = EncodingAndEolTransform.BomPolicy.CopyUtf8OrForce
+                                OutputEncoding = Encoding.UTF7
+                            };
+
+            try
+            {
+                using (var fileProcessor = new FileProcessor())
+                {
+                    var files = fileProcessor.ProcessFilesAsync(args, transform.TransformFileAsync).Result;
+
+                    foreach (var file in files)
+                        Console.WriteLine("{0}", file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
